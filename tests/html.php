170aae2a996ca22c37313cc1327926181657522c
mole <mole@mole-mac.local>
2016-07-10 06:16:59 +0800

    change test sample.

--- tests/html.php
+++ tests/html.php
@@ -1,16 +1,21 @@
-<?php
-
-namespace mole;
-
-class A
-{
-    public $name;
-    public function a()
-    {
-        $a->name
-            ->b
-            ->c
-            ->call();
-    }
-
-}
+                                <div class="row">
+                                    <div class="form-tible -form-special-title col-md-12">
+                                        <?php if ($isRead) { ?>
+                                        <?= $form->field($appModel, 'is_united_states_citizen', ["options" => ["class" => "form-group -form-group-fix-margin"]])->label('Are you a United States citizen?<b>*</b>')->radioList(DictApplicant::$americanCitizen, ['class' => 'row from-radio-wrap', 'item' => function ($index, $label, $name, $checked, $value) {
+                                        $radio = Html::radio($name, $checked, ['value' => $value, "disabled" => "disabled"]);
+                                        $label = Html::label($radio . $label);
+                                        $label = Html::tag('div', $label, ['class' => 'radio -radio-theme -radio-fix-margin']);
+                                        return Html::tag('div', $label, ['class' => 'col-md-6']);
+                                        }]
+                                        )?>
+                                        <?php } else { ?>
+                                        <?= $form->field($appModel, 'is_united_states_citizen', ["options" => ["class" => "form-group -form-group-fix-margin"]])->label('Are you a United States citizen?<b>*</b>')->radioList(DictApplicant::$americanCitizen, ['class' => 'row from-radio-wrap', 'item' => function ($index, $label, $name, $checked, $value) {
+                                        $radio = Html::radio($name, $checked, ['value' => $value]);
+                                        $label = Html::label($radio . $label);
+                                        $label = Html::tag('div', $label, ['class' => 'radio -radio-theme -radio-fix-margin']);
+                                        return Html::tag('div', $label, ['class' => 'col-md-6']);
+                                        }])?>
+                                        <?php } ?>
+                                    </div>
+                                </div>
+                                <span><?php $name = 'jin'; ?></span>
